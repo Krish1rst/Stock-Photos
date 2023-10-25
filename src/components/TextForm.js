509b4c94afe3a8143b2ea23f95c916c229a08d1d
Krishna Singh Yadav <128611710+Krish1rst@users.noreply.github.com>
2023-10-25 15:25:21 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,5 +1,4 @@
 import React,{useState} from 'react'
-import PropTypes from 'prop-types'
 export default function TextForm(props) {
   const handleUpClick=()=>{
     //console.log("uppercase was clicked"+text);
